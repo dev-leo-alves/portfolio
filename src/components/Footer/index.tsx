8583fc93b482ed6250a4785ae32d3a2fedc1f696
Leonardo Alves <leo.alvesoliveira2000@gmail.com>
2025-04-02 18:37:24 -0300

    added new styles to footer, refactor to styled-components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,93 +1,79 @@
-import { Flex, VStack, Heading, Text, Link as ChakraLink } from "@chakra-ui/react"
-import { Link } from "./styles"
+import { Flex, VStack, Heading, Text, Link as ChakraLink, useBreakpointValue, Image } from "@chakra-ui/react"
+import { Link, Container } from "./styles"
 import { scrollToSectionById } from "@/utils/scrollToSectionById"
 import { useTranslation } from "react-i18next";
 import { Contact } from "./Contact";
 export function Footer(){
     const [t] = useTranslation("global");
     const currentYear = new Date().getFullYear();
+
+    const isMobileVersion = useBreakpointValue({
+        base: true,
+        lg: false,
+    });
+
     return(
         <>
         <Contact/>
-            <Flex
-            w="100vw"
-            minH="70vh"
-            px="20"
-            py={["7", "7", "2"]}
-            direction={["column", "column", "row"]}
-            align="center"
+            <Container
         >
             <VStack
-            w={["100%", "100%", "33%"]}
-            h="80%"
-            p="10"
-            color="gray.200"
-            fontFamily="Montserrat"
-            align={["center", "center", "flex-start"]}
-            justify={["center", "center", "flex-start"]}
-            borderColor="gray.800"
-            borderLeftWidth={[0, 0, 2]}
+                className="footer-topic"
             >
-            <Heading
-                fontFamily="Montserrat"
-                fontSize="2xl"
-                color="var(--emerald-500)"
-            >
-                Menu
-            </Heading>
-            <Link onClick={() => scrollToSectionById('about')}>{t("navigation.about")}</Link>
-            <Link onClick={() => scrollToSectionById('professional')}>{t("navigation.professional")}</Link>
-            <Link onClick={() => scrollToSectionById('portfolio')}>{t("navigation.portfolio")}</Link>
-            <Link onClick={() => scrollToSectionById('contact')}>{t("navigation.contact")}</Link>
+                <Heading
+                    fontFamily="Montserrat"
+                    fontSize="2xl"
+                    color="var(--emerald-500)"
+                >
+                    Menu
+                </Heading>
+                <Link onClick={() => scrollToSectionById('about')}>{t("navigation.about")}</Link>
+                <Link onClick={() => scrollToSectionById('professional')}>{t("navigation.professional")}</Link>
+                <Link onClick={() => scrollToSectionById('portfolio')}>{t("navigation.portfolio")}</Link>
+                <Link onClick={() => scrollToSectionById('contact')}>{t("navigation.contact")}</Link>
             </VStack>
+
             <VStack
-            w={["100%", "100%", "33%"]}
-            h="80%"
-            p="10"
-            color="gray.200"
-            fontFamily="Montserrat"
-            align={["center", "center", "flex-start"]}
-            justify={["center", "center", "flex-start"]}
-            textAlign={["center", "center", "start"]}
-            borderColor="gray.800"
-            borderLeftWidth={[0, 0, 2]}
+                className="footer-topic"
             >
-            <Heading
-                 fontFamily="Montserrat"
-                 fontSize="2xl"
-                 color="var(--emerald-500)"
-            >
-                Redes Sociais
-            </Heading>
-            <ChakraLink href="https://www.linkedin.com/in/leonardo-alves-35a7281a0/" target="_blank">
-                Linkedin
-            </ChakraLink>
-            <ChakraLink href="https://github.com/dev-leo-alves/" target="_blank">Github</ChakraLink>
-            <ChakraLink href="https://www.instagram.com/__leo.alves/" target="_blank">Instagram</ChakraLink>
+                <Heading
+                    fontFamily="Montserrat"
+                    fontSize="2xl"
+                    color="var(--emerald-500)"
+                >
+                    Redes Sociais
+                </Heading>
+                <ChakraLink href={t("links.linkedin")} target="_blank">
+                    Linkedin
+                </ChakraLink>
+                <ChakraLink href={t("links.github")} target="_blank">Github</ChakraLink>
+                <ChakraLink href={t("links.instagram")} target="_blank">Instagram</ChakraLink>
             </VStack>
+
             <VStack
-            w={["100%", "100%", "33%"]}
-            h="80%"
-            p="10"
-            color="gray.200"
-            fontFamily="Montserrat"
-            align={["center", "center", "flex-start"]}
-            justify={["center", "center", "flex-start"]}
-            borderColor="gray.800"
-            borderLeftWidth={[0, 0, 2]}
-            textAlign={["center", "center", "start"]}
+                className="footer-topic"
             >
-            <Heading
-                fontFamily="Montserrat"
-                fontSize="2xl"
-                color="var(--emerald-500)"
-            >
-                Entre em Contato
-            </Heading>
-            <Text>+55 (11) 97555-1708</Text>
-            <Text>leo.alvesoliveira2000@gmail.com</Text>
+                <Heading
+                    fontFamily="Montserrat"
+                    fontSize="2xl"
+                    color="var(--emerald-500)"
+                >
+                    Entre em Contato
+                </Heading>
+                <Text>+55 (11) 97555-1708</Text>
+                <Text>leo.alvesoliveira2000@gmail.com</Text>
             </VStack>
-        </Flex>
+
+            {!isMobileVersion && <>
+                <VStack>
+                <Image src="./src/assets/images/rayquaza-rocks.gif" h="20rem" />
+                <Text fontSize="1.3rem">Fim. 🤘</Text>
+                </VStack>
+            </>
+            }
+
+
+        </Container>
         <Flex
             w="100vw"
             align="center"
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -9,3 +9,44 @@ export const Link = styled.button`
     color: var(--emerald-500);
   }
 `;
+
+export const Container = styled.div`
+  
+    width: 100vw;
+    min-height: 70vh;
+    padding: 20px;
+    flex-direction: row;
+    align-items: center;
+    
+  .footer-topic{
+    width:33%;
+    height:80%;
+    color: var(--gray-200);
+    font-family:"Montserrat";
+    align-items: flex-start;
+    justify-content: flex-start;
+    background-image: linear-gradient(to right, var(--emerald-500) 20%, transparent 70%);
+    background-size: 100% 2px;
+    background-repeat: no-repeat;
+    background-position: top;
+    padding: 15px 10px 10px 0px;
+    text-align: start;
+  }
+
+  @media (min-width: 200px) and (max-width: 980px) {
+    padding: 20px 7px;
+    flex-direction: column;
+
+    .footer-topic{
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+    }
+     
+  }
+
+
+
+
+`;
